31497e582616eebb688917e801a481ce0b31214d
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-05-09 16:25:04 +0300

    worked on the dictionary synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import Synonyms from "./Synonyms"
 
 export default function Meaning(props){
     console.log(props.meaning)
@@ -10,8 +11,12 @@ export default function Meaning(props){
             
             {
                 return(<div key={index}>
-                <p>{definition.definition}
-                    <br/><em>{definition.example}</em>
+                <p><strong>definition:</strong>{definition.definition}
+                    <br/>
+                    <strong>example:</strong>
+                    <em>{definition.example}</em>
+                 
+                    <Synonyms synonyms={definition.synonyms}/>
                     </p>
                 </div>
                 );
