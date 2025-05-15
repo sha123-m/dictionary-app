9c40e5972d5f8a3010db27a7fd1a8317cedc2e60
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-05-15 21:16:15 +0300

    updates on redirect

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-       
+      <div className="Synonyms">
+        <strong>Similar:</strong>
+
+        <ul>
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
           })}
         </ul>
-    
+      </div>
     );
   } else {
     return null;
