8ff8638ffbf444648c32542674f51663163d1196
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-05-15 12:31:13 +0300

    worked on phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
